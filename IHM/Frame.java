f86083aea5b8ca531986257d72c95e8bba1ec10d
kevin aubriet <kevin97@wanadoo.fr>
2017-03-02 10:43:57 +0100

    mis en place du menu

--- IHM/Frame.java
+++ IHM/Frame.java
@@ -1,6 +1,8 @@
 package IHM;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 import Projet.Projet;
 
@@ -8,12 +10,32 @@ public class Frame extends JFrame {
 
 	public static Projet p;
 	
+	/*private JMenuBar menuBar = new JMenuBar();
+	private JMenu menu1 = new JMenu("Fichier");
+	private JMenu menu2 = new JMenu("Sous ficher");
+	private JMenu menu3 = new JMenu("Edition");*/
+
+	
 	public Frame() {
 		this.setSize(500, 200);
 		this.setContentPane(new PanelPrincipal());
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		//this.setUndecorated(true);
+		
+/*---------------------------------------------------Menu-------------------------------------------------------- */ 	
+     
+      	
+       	//this.menuBar.add(menu1);
+       // this.menuBar.add(menu2);
+        //this.setJMenuBar(menuBar);
+        
+        /*JFrame frame = (JFrame)this.getTopLevelAncestor();
+        frame.setJMenuBar(menuBar);	*/
+       	
+       	
+  /*---------------------------------------------------Menu---------------------------------------------------------*/ 
+		
 		this.setVisible(true);
 	}
 	
--- gestionTexture/GestionTexture.java
+++ gestionTexture/GestionTexture.java
@@ -12,6 +12,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -80,6 +81,8 @@ public class GestionTexture implements Serializable{
 		
 		JPanel j = new JPanel();
 		GridLayout gl = new GridLayout(nbLigne, nbColonne);
+		j.setBackground(new Color(241,241,241));
+
 		gl.setHgap(30);
 		gl.setVgap(30);
 		j.setLayout(gl);
